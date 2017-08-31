d04548d4fe859e83b9abd7d91b44e7dddd674760
Jose Ruiz <jruizj6@gmail.com>
2017-08-30 21:46:31 -0600

    secuencias strings

--- Tarea1.py
+++ Tarea1.py
@@ -31,6 +31,69 @@ obtiene el largo de una hilera
 @param {string}   hilera - hilera a utilizar
 @return {int} largo de la hilera
 """
-def largoHilera(hilera):
+def obtenerLargoHilera(hilera):
 	return len(hilera)
+
+"""
+obtiene el i-esimo elemento de una hilera
+@param {string}   hilera - hilera a utilizar
+@param {int} posicion - posicion en la hilera del elemento a obtener
+@return {char} i-esimo elemento de la hilera
+"""
+def obtenerElementoHilera(hilera, posicion):
+	return hilera[posicion-1]
+
+"""
+determina si un string es sub secuencia de una secuencia
+@param {string} secuencia - secuencia a utilizar
+@param {string} subSecuencia - sub secuencia que se quiere evaluar
+@return {bool} resultado de la comparacion
+"""
+def esSubSecuencia(secuencia,subSecuencia):
+	i = 0
+	j = 0
+	largoSecuencia = obtenerLargoHilera(secuencia)
+	largoSubSecuencia = obtenerLargoHilera(subSecuencia)
+	while i < largoSecuencia and j < largoSubSecuencia:
+		if secuencia[i] == subSecuencia[j]:
+			j = j + 1
+		i = i + 1
+	return j==largoSubSecuencia
+
+"""
+determina si un string es super secuencia de una secuencia
+@param {string} secuencia - secuencia a utilizar
+@param {string} superSecuencia - super secuencia que se quiere evaluar
+@return {bool} resultado de la comparacion
+"""
+def esSuperSecuencia(secuencia,superSecuencia):
+	i = 0
+	j = 0
+	largoSecuencia = obtenerLargoHilera(secuencia)
+	largoSuperSecuenica = obtenerLargoHilera(superSecuencia)
+	while i < largoSecuencia and j < largoSuperSecuenica:
+		if secuencia[i] == superSecuencia[j]:
+			i = i + 1
+		j = j + 1
+	return i==largoSecuencia
+
+"""
+determina si un string es sub string de otra
+@param {string} string - secuencia a utilizar
+@param {string} subString - sub string que se quiere evaluar
+@return {bool} resultado de la comparacion
+"""
+def esSubString(string,subString):
+	return subString in string
+
+"""
+determina si un string es super string de otra
+@param {string} string - secuencia a utilizar
+@param {string} superString - super string que se quiere evaluar
+@return {bool} resultado de la comparacion
+"""
+def esSuperString(string, superString):
+	return string in superString
+
+
 
